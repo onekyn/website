8999bb427c76b1809c4cc12e8566e8a14b736c02
kynez <kynez.dev@gmail.com>
2025-03-01 01:50:14 +0100

    Bare-bones setup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 export default (config) => {
     config.setInputDirectory("./site/");
+    config.setOutputDirectory("./dist/");
 
     // Copy the contents of the `public` folder to the output folder
     config.addPassthroughCopy({ "./public/": "/" });
