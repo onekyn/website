d1378b61321c28f226d174a13409c881be178b9c
kynez <kynez.dev@gmail.com>
2025-03-01 01:52:22 +0100

    GH Pages wants me to put everything in the docs folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 export default (config) => {
     config.setInputDirectory("./site/");
-    config.setOutputDirectory("./dist/");
+    config.setOutputDirectory("./docs/");
 
     // Copy the contents of the `public` folder to the output folder
     config.addPassthroughCopy({ "./public/": "/" });
